41613395a4c84a2dc2f71f539af9d57af6a54bea
Marco Vignotto <marcovignotto81@gmail.com>
2021-06-25 11:50:39 +0200

    Fixed Redux Dev Tools

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const rootReducer = combineReducers({
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(ReduxThunk)
+    applyMiddleware(ReduxThunk),
     window.navigator.userAgent.includes("Chrome")
       ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
           window.__REDUX_DEVTOOLS_EXTENSION__()
